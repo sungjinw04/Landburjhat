1609e7c0fcf73a8f8231811b96b75dfeb536bece
SKARMAAN <83514208+SKARMAAN@users.noreply.github.com>
2024-10-24 18:40:59 +0530

    Update add.py

--- Lundgrow/modules/admin/add.py
+++ Lundgrow/modules/admin/add.py
@@ -5,18 +5,33 @@ from Lundgrow.database import special_user_collection
 
 @Bot.on_message(filters.command("add") & filters.reply)
 async def add_special_user(client, message):
-    if int(message.from_user.id) not in DEV_ID  and int(message.from_user.id) not in OWNER_ID:
+    if int(message.from_user.id) not in DEV_ID and int(message.from_user.id) not in OWNER_ID:
+        return
+
     target_user_id = message.reply_to_message.from_user.id
     target_first_name = message.reply_to_message.from_user.first_name
 
-    
     if special_user_collection.find_one({"user_id": target_user_id}):
-        await message.reply(f"{target_first_name} is already in the special dick grower list.")
+        await message.reply(f"{target_first_name} is already in the special list.")
         return
 
-    
     special_user_collection.insert_one({"user_id": target_user_id})
 
-    
-    await message.reply(f"Successfully added [{target_first_name}](tg://user?id={target_user_id}) to the special dick grower list.", parse_mode=enums.ParseMode.MARKDOWN)
+    await message.reply(f"Successfully added [{target_first_name}](tg://user?id={target_user_id}) to the special list.", parse_mode=enums.ParseMode.MARKDOWN)
 
+
+@Bot.on_message(filters.command("remove") & filters.reply)
+async def remove_special_user(client, message):
+    if int(message.from_user.id) not in DEV_ID and int(message.from_user.id) not in OWNER_ID:
+        return
+
+    target_user_id = message.reply_to_message.from_user.id
+    target_first_name = message.reply_to_message.from_user.first_name
+
+    if not special_user_collection.find_one({"user_id": target_user_id}):
+        await message.reply(f"{target_first_name} is not in the special list.")
+        return
+
+    special_user_collection.delete_one({"user_id": target_user_id})
+
+    await message.reply(f"Successfully removed [{target_first_name}](tg://user?id={target_user_id}) from the special list.", parse_mode=enums.ParseMode.MARKDOWN)
