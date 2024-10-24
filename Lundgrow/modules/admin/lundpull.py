7dded473cef5a93052ea6c94a58f563dc5eba1f3
sungjinw04 <abhishekpandit4224@gmail.com>
2024-10-24 18:19:28 +0530

    Update lundpull.py

--- Lundgrow/modules/admin/lundpull.py
+++ Lundgrow/modules/admin/lundpull.py
@@ -6,7 +6,7 @@ from time import sleep
 import asyncio
 
 from pyrogram import filters
-from Looter import Bot as app
+from Lundgrow import Bot
 from ...config import DEV_ID, OWNER_ID
 
 @app.on_message(filters.command("lundpull"))
