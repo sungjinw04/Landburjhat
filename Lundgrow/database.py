7c9d82ccd143fba7af2dc59a25cdc91fb5f70305
SKARMAAN <83514208+SKARMAAN@users.noreply.github.com>
2024-10-24 16:29:38 +0530

    Create database.py

---
+++ Lundgrow/database.py
@@ -0,0 +1,9 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from . import Config
+from . import Bot as app
+mongo_client = AsyncIOMotorClient(Config.MONGO_URI)
+
+db = mongo_client['Lundgrow']
+user_collection = db["users"]
+special_user_collection = db["spusers"]
+group_collection = db["groups"]
