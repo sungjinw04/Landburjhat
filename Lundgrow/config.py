af169a0532ebc34f300db08ccfbcfebf4b3346cf
sungjinw04 <abhishekpandit4224@gmail.com>
2024-10-24 16:14:33 +0530

    Update config.py

--- Lundgrow/config.py
+++ Lundgrow/config.py
@@ -1,10 +1,10 @@
-OWNER_ID = 
-TOKEN = ""
-MONGO_URI = ""
-SUPPORT = ""
-SUPPORT_CHANNEL = ""
-API_ID = 
-API_HASH = ""
+OWNER_ID = 7392339658
+TOKEN = "8149074708:AAHWW40GU1Yi93ZGT1r-o0W7Fadb9npzmT4"
+MONGO_URI = "mongodb+srv://Sungjinwoo4:sung4224@cluster0.ayaos.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+SUPPORT = "eldian_empire"
+SUPPORT_CHANNEL = "eldian_networks"
+API_ID = 28620311
+API_HASH = "3b5c4ed0598e48fc1ab552675555e693"
 JOINLOGS = 
 LEAVELOGS = 
-BOT_USERNAME = ""
+BOT_USERNAME = "dick_grower_bot"
