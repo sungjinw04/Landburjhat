2b331a8ae645f2c9e473f659dba525ffcc0de742
SKARMAAN <83514208+SKARMAAN@users.noreply.github.com>
2024-10-24 15:29:02 +0530

    Create __init__.py

---
+++ Lundgrow/__init__.py
@@ -0,0 +1,30 @@
+import logging
+from pyrogram import Client, idle
+from motor.motor_asyncio import AsyncIOMotorClient
+from .config import OWNER_ID, TOKEN, MONGO_URI, SUPPORT, SUPPORT_CHANNEL, API_ID, API_HASH, JOINLOGS, LEAVELOGS, BOT_USERNAME
+
+class Config:
+    OWNER_ID = OWNER_ID
+    TOKEN = TOKEN
+    MONGO_URI = MONGO_URI
+    SUPPORT = SUPPORT
+    SUPPORT_CHANNEL = SUPPORT_CHANNEL
+    API_ID = API_ID
+    API_HASH = API_HASH
+    JOINLOGS = JOINLOGS
+    LEAVELOGS = LEAVELOGS
+    BOT_USERNAME = BOT_USERNAME
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+
+Bot = Client(
+    "lundgrow_bot",
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    bot_token=Config.TOKEN
+)
