15e5acf37e59ef6d22794e0e6454cab9c695ade1
SKARMAAN <83514208+SKARMAAN@users.noreply.github.com>
2024-10-24 22:12:39 +0530

    Update decorators.py

--- Lundgrow/decorators.py
+++ Lundgrow/decorators.py
@@ -1,8 +1,8 @@
 from functools import wraps
 from pyrogram.types import Message
-from Lundgrow.database import blacklist_collection  # Assuming you have a collection to store blacklisted chat IDs
+from Lundgrow.database import blacklist_chat, blacklist_user 
 
-def blacklist_check(func):
+def blacklist_chat(func):
     @wraps(func)
     async def wrapper(client, message: Message, *args, **kwargs):
         chat_id = message.chat.id
@@ -14,4 +14,16 @@ def blacklist_check(func):
             return  
         return await func(client, message, *args, **kwargs)
     
+    return wrapper
+
+def blacklist_user(func):
+    @wraps(func)
+    async def wrapper(client, message, *args, **kwargs):
+        user_id = message.from_user.id
+        # Check if the user is banned
+        banned_user = await blacklist_user.find_one({"user_id": user_id})
+        if banned_user:
+            await message.reply("You are banned from using this bot.")
+            return
+        return await func(client, message, *args, **kwargs)
     return wrapper
